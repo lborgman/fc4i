73ee51bcbbf4314b176daee1820af21da285ebce
lborgman <lennart.borgman@gmail.com>
2024-08-07 15:06:03 +0200

    Add anchors-with-base.js Fix loading errors in init-error.js

--- init-error.js
+++ init-error.js
@@ -1,35 +1,41 @@
 // @ts-check
-const INIT_ERROR_VER = "0.1.0";
+const INIT_ERROR_VER = "0.1.2";
 console.log(`here is init-error.js ${INIT_ERROR_VER}`);
 
 {
-    const displayError = (evt) => {
-        setTimeout(() => {
-            const { type, message, reason, filename, lineno } = evt;
-            const msg = `${type}: ${message || reason}, ${filename || ""}:${lineno || ""} `;
-            console.log("in displayError timeout", msg, evt);
-            const dlg = document.createElement("dialog");
-            const h2 = document.createElement("h2");
-            dlg.appendChild(h2);
-            const div = document.createElement("div");
-            dlg.appendChild(div);
-            const btn = document.createElement("button");
-            dlg.appendChild(btn);
+    /**
+     * 
+     * @param {ErrorEvent} evt 
+     */
+    const doDisplay = (evt) => {
+        const { type, message, reason, filename, lineno } = evt;
+        const msg = `${type}: ${message || reason}, ${filename || ""}:${lineno || ""} `;
+        console.log("in displayError timeout", msg, evt);
+        const dlg = document.createElement("dialog");
+        const h2 = document.createElement("h2");
+        dlg.appendChild(h2);
+        const div = document.createElement("div");
+        dlg.appendChild(div);
+        const btn = document.createElement("button");
+        dlg.appendChild(btn);
 
-            h2.textContent = "Error";
-            div.textContent = msg;
-            btn.textContent = "Close";
+        h2.textContent = "Error";
+        div.textContent = msg;
+        btn.textContent = "Close";
 
-            // @ts-ignore
-            dlg.style = `
-            background: red;
-            color: black;
-            font-size: 1rem;
-            `;
-            btn.addEventListener("click", evt => dlg.close());
-            document.body.appendChild(dlg);
-            dlg.showModal();
-        }, 100);
+        // @ts-ignore
+        dlg.style = `
+        background: red;
+        color: black;
+        font-size: 1rem;
+        `;
+        btn.addEventListener("click", evt => dlg.close());
+        document.body.appendChild(dlg);
+        dlg.showModal();
+    };
+    const displayError = (evtError) => {
+        if (document.readyState != "loading") { doDisplay(evtError); return; }
+        document.addEventListener("DOMContentLoaded", () => { doDisplay(evtError); });
     }
     window.addEventListener("error", evt => { displayError(evt); });
     window.addEventListener("unhandledrejection", evt => { displayError(evt); });
