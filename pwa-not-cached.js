7bb34fefaf6942a5e6f042e4e23b28fbac954c18
lborgman <lennart.borgman@gmail.com>
2024-08-10 21:09:03 +0200

    Don't throw for wrong params to pwa-not-cached.js

--- pwa-not-cached.js
+++ pwa-not-cached.js
@@ -37,10 +37,8 @@ let ourUrlSW;
 const urlModule = new URL(import.meta.url);
 const params = [...urlModule.searchParams.keys()];
 const parNc = "PWAnocacheRand"
-if (params.length != 1 || params[0] != parNc) { throw Error(`Should be only 1 parameter, "${parNc}"`); }
+if (params.length != 1 || params[0] != parNc) { console.error(`Should be only 1 parameter, "${parNc}"`); }
 
-if (document.currentScript) throw Error("import .currentScript"); // is module
-if (!import.meta.url) throw Error("!import.meta.url"); // is module
 
 
 
