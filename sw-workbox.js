4ffafda04da703a098c09ff60f22bba54653c084
lborgman <lennart.borgman@gmail.com>
2024-09-24 17:09:26 +0200

    Rename the file rem10m1h.css to fc4i.css

--- sw-workbox.js
+++ sw-workbox.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.4.1506";
+const SW_VERSION = "0.4.1507";
 
 
 // https://www.npmjs.com/package/workbox-sw
