91c7e3b9eacd1f56324b5367299fa98b995ae02b
lborgman <lennart.borgman@gmail.com>
2024-09-24 16:56:13 +0200

    Renamve rem10m1h.css to fc4i.css

--- sw-input.js
+++ sw-input.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.4.1505";
+const SW_VERSION = "0.4.1506";
 
 
 // https://www.npmjs.com/package/workbox-sw
