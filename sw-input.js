8e368cabca820aeb686f0a458a8323a7a5ea1bfe
lborgman <lennart.borgman@gmail.com>
2024-10-06 13:42:04 +0200

    Bump version number

--- sw-input.js
+++ sw-input.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.4.1566";
+const SW_VERSION = "0.4.1567";
 
 
 // https://www.npmjs.com/package/workbox-sw
