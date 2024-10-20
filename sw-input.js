50200f0b9265c6d3272b19a5c8360e6690f57316
lborgman <lennart.borgman@gmail.com>
2024-10-20 16:21:31 +0200

    Set overflow:hidden in .jmnode-overflow

--- sw-input.js
+++ sw-input.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.4.1776";
+const SW_VERSION = "0.4.1778";
 
 
 // https://www.npmjs.com/package/workbox-sw
