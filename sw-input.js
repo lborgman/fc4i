6a186e566d3ba01609ce4a5a2f2e626279943a3d
lborgman <lennart.borgman@gmail.com>
2024-10-31 00:30:37 +0100

    jmnode.selected outline

--- sw-input.js
+++ sw-input.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.4.1901";
+const SW_VERSION = "0.4.1902";
 
 
 // https://www.npmjs.com/package/workbox-sw
